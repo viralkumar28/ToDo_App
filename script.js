3a545d494cf03f59c4bdc058263e6cb07d6bbda4
viralkumar28 <viralkumarmaurya@gmail.com>
2021-10-14 16:25:10 +0530

    Update script.js

--- script.js
+++ script.js
@@ -6,3 +6,11 @@ function get_todos(){
     }
     return todos;
 }
+function add(){
+    var task = document.getElementById('task').value;
+    var todos = get_todos();
+    todos.push(task);
+    localStorage.setItem('todo', JSON.stringify(todos));
+    show();
+    return false;
+}
