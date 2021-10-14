d8dfdee303769de28d6a9be501c077ee34c4803c
viralkumar28 <viralkumarmaurya@gmail.com>
2021-10-14 16:17:10 +0530

    Update script.js

---
+++ script.js
@@ -0,0 +1,8 @@
+function get_todos(){
+    var todos = new Array;
+    var todos_str = localStorage.getItem('todo');
+    if(todos_str !== null){
+        todos = JSON.parse(todos_str);
+    }
+    return todos;
+}
