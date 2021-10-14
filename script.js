5e2d5a969ee0b4806306d73bcd96daef0bc77f4b
viralkumar28 <viralkumarmaurya@gmail.com>
2021-10-14 16:36:33 +0530

    Update script.js

--- script.js
+++ script.js
@@ -27,3 +27,21 @@ function remove(){
     show();
     return false;
 }
+function show(){
+    var todos = get_todos();
+    var html = '<ul>';
+    for(var i=0; i<todos.length;i++){
+        html += '<li>' + todos[i] + '<button class ="remove" id=""' + i + '">Delete</button> </li>';
+
+    };
+    html += '</ul>';
+
+    document.getElementById('todos').innerHTML = html;
+    var button = document.getElementsByClassName('remove');
+    for(var i=0; i<buttons.length;i++){
+        buttons[i].addEventListener('click', remove);
+    };
+}
+
+document.getElementById('add').addEventListener('click', add);
+show();
